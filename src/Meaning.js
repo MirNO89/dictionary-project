04c1c753e993eeba35c3823329c086e915407ac7
MirNO89 <108218701+MirNO89@users.noreply.github.com>
2022-10-05 16:36:45 +0200

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Synonyms from "./Synonyms";
+
 export default function Meaning(props) {
   return (
     <div className="Meaning">
@@ -7,11 +9,13 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function(definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
+            <strong>Definition: </strong>
+            {definition.definition}
+            <br />
+            <strong>Examples: </strong>
+            <em>{definition.example}</em>
+            <br />
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
